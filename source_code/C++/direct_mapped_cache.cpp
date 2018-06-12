da009709c571de21d783e16f70df87c6fee0cb99
purinhow7 <dglpw7546@gmail.com>
2018-06-12 19:23:40 +0800

    Comment_sourceCpp
    add some commet in direct_mapped_cache.cpp

--- source_code/C++/direct_mapped_cache.cpp
+++ source_code/C++/direct_mapped_cache.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 struct cache_content
 {
-	bool v;
+	bool v;		//verify bit
 	unsigned int tag;
     // unsigned int	data[16];
 };
@@ -15,7 +15,7 @@ const int K = 1024;
 
 double log2(double n)
 {  
-    // log(n) / log(2) is log2.
+    // log(n) / log(2) is log2.	log with base 2
     return log(n) / log(double(2));
 }
 
@@ -24,29 +24,29 @@ void simulate(int cache_size, int block_size)
 {
 	unsigned int tag, index, x;
 
-	int offset_bit = (int)log2(block_size);
+	int offset_bit = (int)log2(block_size);		//2^(offset_bit) bytes in one block
 	int index_bit = (int)log2(cache_size / block_size);
-	int line = cache_size >> (offset_bit);
+	int line = cache_size >> (offset_bit);	// cache/2^(offset_bit) is "number of block"
 
 	cache_content *cache = new cache_content[line];
 	
     cout << "cache line: " << line << endl;
 
-	for(int j = 0; j < line; j++)
-		cache[j].v = false;
+	for(int j = 0; j < line; j++)	//instart, every line is empty
+		cache[j].v = false;	
 	
     FILE *fp = fopen("test.txt", "r");  // read file
 	
 	while(fscanf(fp, "%x", &x) != EOF)
     {
-		cout << hex << x << " ";
-		index = (x >> offset_bit) & (line - 1);
-		tag = x >> (index_bit + offset_bit);
+		cout << hex << x << " ";	//hexadecimal base
+		index = (x >> offset_bit) & (line - 1);	//filter the index bits
+		tag = x >> (index_bit + offset_bit);	//filter the tag bits
 		if(cache[index].v && cache[index].tag == tag)
 			cache[index].v = true;    // hit
 		else
         {
-			cache[index].v = true;  // miss
+			cache[index].v = true;  // miss, update block
 			cache[index].tag = tag;
 		}
 	}
